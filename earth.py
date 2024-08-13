612ae55aad1a0c3faab18798daddd8c66da712b3
JoacoDerito <162723144+JoacoDerito@users.noreply.github.com>
2024-08-13 16:39:42 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -3,5 +3,3 @@ def earth():
     y = "Barbados" 
     print("The result of",x, "comes first in the dictionary than",y, "is", x < y)
     print("The result of",y, "comes first in the dictionary than",x, "is", y < x)
-
-earth()
